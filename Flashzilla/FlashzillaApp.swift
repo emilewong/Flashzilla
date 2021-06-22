af07d4f3e88bbfa9d9e5a662d7607ba94571eb20
Emile Wong <emilewong@outlook.com>
2021-06-22 15:38:07 +0800

    Initial Commit

---
+++ Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Emile Wong on 22/6/2021.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
